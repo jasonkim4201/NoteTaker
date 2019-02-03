2fa24706142ed310f4d284e06d31b8a6deac25f9
Jason Kim <jasonkim4201@gmail.com>
2019-02-03 17:46:01 -0500

    found a way to update page without refreshing

--- public/assets/JAVASCRIPT.js
+++ public/assets/JAVASCRIPT.js
@@ -60,6 +60,50 @@ $(document).ready(function(){
   
 
 
+    $.ajax({
+      url: "/api/typedNotes",
+      method: "GET",
+      data: noteTaken
+    }).then(reminder => {
+      console.log(reminder);
+        //my "solution" to showing the most recent note posted without refreshing.      
+      for (var i = (reminder.length - 1); i < reminder.length; i++) {
+        const noteCol = $(`<div class="col-3">`);
+        const stickyNote = $(`<div class="card my-2 bg-warning" style="width: 18rem;">`);
+        const stickyNoteBody = $(`<div class="card-body">`);
+        const stickyNoteTitle = $(`<h5 class="card-title">`);
+        const stickyNoteText = $(`<p class="card-text">`);
+  
+        $("#stickyNotes").append(noteCol);
+        noteCol.append(stickyNote);
+        stickyNote.append(stickyNoteBody);
+  
+        stickyNoteBody.append(
+          stickyNoteTitle,
+          stickyNoteText
+        );
+    
+        $(stickyNoteTitle).html(`${reminder[i].title} <span class="float-right delete" id =${i}>🗑️</span>`);
+        $(stickyNoteText).text(reminder[i].note_text);
+  
+       $(".delete").on("click", function() {
+         //delete from database
+        $(this).closest(".col-3").remove();
+        
+       })
+  
+      } //end of loop bracket
+  
+    })// end of GET ajax
+
+
+
+
+
+
+
+
+
   }); //end on on click
 
 
